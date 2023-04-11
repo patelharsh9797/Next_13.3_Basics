2296f6512e33e608e01f445fcfa9610c2a96147b
Harsh Patel <105432105+patelharsh9797@users.noreply.github.com>
2023-04-11 11:30:46 +0530

    Init + Added Nested Routes in About

---
+++ app/about/layout.jsx
@@ -0,0 +1,10 @@
+const AboutLayout = ({ children }) => {
+  return (
+    <div>
+      <h1>this is in layout page</h1>
+      {children}
+    </div>
+  );
+};
+
+export default AboutLayout;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,9 @@
+const Team = () => {
+  return (
+    <div>
+      <h1>Team Page</h1>
+    </div>
+  );
+};
+
+export default Team;
